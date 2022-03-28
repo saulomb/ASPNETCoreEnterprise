5fbc3eb6702d490bd9c8f291addb7a3732f47b25
saulomb <saulomb@gmail.com>
2022-03-27 21:03:19 -0300

    Concluindo o BFF de Compras

---
+++ src/building blocks/NSE.WebAPI.Core/Extensions/PollyExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NSE.Bff.Compras.Extensions
+{
+    public static class PollyExtensions
+    {
+
+        public static AsyncRetryPolicy<HttpResponseMessage> EsperarTentar()
+        {
+            var retry = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10),
+                });
+
+            return retry;
+        }
+    }
+}
--- src/web/NSE.WebApp.MVC/Models/ErrorViewModel.cs
+++ src/web/NSE.WebApp.MVC/Models/ErrorViewModel.cs
@@ -9,25 +9,25 @@ namespace NSE.WebApp.MVC.Models
         public string Mensagem { get; set; }
     }
 
-    public class ResponseResult
-    {
-        public ResponseResult()
-        {
-            Errors = new ResponseErrorMessages();
-        }
+    //public class ResponseResult
+    //{
+    //    public ResponseResult()
+    //    {
+    //        Errors = new ResponseErrorMessages();
+    //    }
 
-        public string Title { get; set; }
-        public int Status { get; set; }
-        public ResponseErrorMessages Errors { get; set; }
-    }
+    //    public string Title { get; set; }
+    //    public int Status { get; set; }
+    //    public ResponseErrorMessages Errors { get; set; }
+    //}
 
-    public class ResponseErrorMessages
-    {
-        public ResponseErrorMessages()
-        {
-            Mensagens = new List<string>();
-        }
+    //public class ResponseErrorMessages
+    //{
+    //    public ResponseErrorMessages()
+    //    {
+    //        Mensagens = new List<string>();
+    //    }
 
-        public List<string> Mensagens { get; set; }
-    }
+    //    public List<string> Mensagens { get; set; }
+    //}
 }
